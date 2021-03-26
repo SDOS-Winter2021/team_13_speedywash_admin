06454120668b88ca8dd52d7080c3956c7c3666b1
Lord Hardik <hardik18391@iiitd.ac.in>
2021-03-26 17:52:21 +0530

    Added Create New Admin Functionality Fixed #10

--- src/Components/CreateNewAdminView/CreateNewAdminView.js
+++ src/Components/CreateNewAdminView/CreateNewAdminView.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import "./styles.css"
+import firebase from "firebase";
+import { Button } from '@material-ui/core';
+import KEYS from '../../configs/KEYS';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
 
 function CreateNewAdminView() {
+
+    const [snackObject, setsnackObject] = useState({
+        open: false,
+        message: "User Created Successfully",
+        type: "success",
+    })
+
+    const [adminDetails, setAdminDetails] = useState({
+        Email: "",
+        Name: "",
+        Roll: ""
+    })
+
+    function handleSnackBarClosing() {
+        setsnackObject((prev) => {
+            return { ...prev, open: false }
+        })
+    }
+
+    function handleSubmitAction() {
+        firebase.firestore()
+            .collection(KEYS.DATABASE.COLLECTIONS.ADMINS)
+            .doc(adminDetails.Email)
+            .set(adminDetails)
+            .then(() => {
+                setAdminDetails({
+                    Email: "",
+                    Name: "",
+                    Roll: ""
+                });
+                setsnackObject((prev) => (
+                    { ...prev, open: true }
+                ))
+            })
+    }
+
     return (
-        <div>
-            <h1>
-                Create New Admin View
-            </h1>
+        <div class={"formDiv"} >
+            <TextField required value={adminDetails.Email} onChange={(event) => { setAdminDetails((prev) => ({ ...prev, Email: event.target.value })) }} id="standard-basic" label="Email" autoComplete="off" autoFocus={true} />
+            <br />
+            <TextField required value={adminDetails.Name} onChange={(event) => { setAdminDetails((prev) => ({ ...prev, Name: event.target.value })) }} id="standard-basic" label="Name" autoComplete="off" />
+            <br />
+            <TextField required value={adminDetails.Roll} onChange={(event) => { setAdminDetails((prev) => ({ ...prev, Roll: event.target.value })) }} id="standard-basic" label="Roll" autoComplete="off" />
+            <br /><br />
+            <Button variant="contained" disabled={adminDetails.Email == "" || adminDetails.Name == "" || adminDetails.Roll == ""} color="primary" onClick={handleSubmitAction} >Create Account</Button>
+            <Snackbar open={snackObject.open} autoHideDuration={3000}
+                onClose={handleSnackBarClosing}>
+                <Alert onClose={handleSnackBarClosing} severity={snackObject.type}>
+                    {snackObject.message}
+                </Alert>
+            </Snackbar>
         </div>
     )
 }
