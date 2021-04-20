5bf643c35479b2538fb4b8bfe4a8574d3e13ded1
aman18327 <aman18327@iiitd.ac.in>
2021-04-20 21:52:40 +0530

    Admin can now upload Advertisement images for users Fixed #9

--- src/Components/UpdateAdvertisementView/UpdateAdvertisementView.js
+++ src/Components/UpdateAdvertisementView/UpdateAdvertisementView.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import "./styles.css"
+import { Button } from '@material-ui/core';
+import firebase from "firebase";
 
 function UpdateAdvertisementView() {
+    const [newImage, setnewImage] = useState({});
+    const [advertisements, setAdvertisements] = useState("");
+    const [newURL, setnewURL] = useState("");
+    
+    // store the selected image in local storage
+    const handleChange = e => {
+        if(e.target.files[0]) {
+            setnewImage(e.target.files[0]);
+        }
+    };
+
+    // upload the image to firebase
+    const handleUploadClick = () => {
+        console.log(newImage);
+        const refer = firebase.storage().ref('advertisements/image').put(newImage);
+        refer.on(
+            "state_changed",
+            snapshot => {},
+            error => {
+                console.log(error);
+            },
+            () => {
+                firebase.storage()
+                .ref("advertisements")
+                .child("image")
+                .getDownloadURL()
+                .then(url => {
+                    console.log(url);
+                    setnewURL(url);
+                })
+            }
+        );
+    };
+
     return (
         <div>
-            <h1>
-                Update Advertisement View
-            </h1>
+            <h1 class="title">Advertisements</h1>
+            <h3 class="inlineElement">Edit Advertisement</h3>
+            <input type="file" onChange={handleChange}/>
+            <button onClick={handleUploadClick}>Upload</button>
         </div>
     )
 }
