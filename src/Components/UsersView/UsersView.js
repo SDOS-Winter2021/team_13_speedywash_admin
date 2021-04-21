e89fd9f9cb66a45758688e9535db49cb4fc79222
Prasham2302 <prasham18359@iiitd.ac.in>
2021-04-21 19:05:59 +0530

    Linked users page to the database, Fixed #33

--- src/Components/UsersView/UsersView.js
+++ src/Components/UsersView/UsersView.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ListUsers from './ListUsers'
 import UserDetails from './UserDetails'
+import firebase from "firebase";
+import KEYS from '../../configs/KEYS';
+
 function UsersView() {
+
+    const [users, setUsers] = useState('');
+    const [selectedUser, setSelectedUser] = useState('');
+    
+    useEffect(()=>
+    {
+        firebase.firestore().collection(KEYS.DATABASE.COLLECTIONS.USERS).onSnapshot((records) => {
+            let userList = [];
+            setUsers([]);
+            records.forEach((doc)=>{       
+                userList.push(doc.data());
+            }) 
+            setUsers(userList);
+        });
+    },[])    
+    console.log(users);
+
     return (
         <div>
             <h1 className="title">User's Info</h1>
